5d6c060d94970f286be24eb7114abf81872da6b3
AbdulazizGhoniem7 <abdulazizghoniem7@gmail.com>
2022-12-12 17:23:14 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main block
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
