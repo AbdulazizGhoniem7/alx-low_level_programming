fcee22c7b29f22b4996cc90c9c4e7d8e2f7ec291
AbdulazizGhoniem7 <abdulazizghoniem7@gmail.com>
2022-12-12 17:04:33 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Enrtry point
+ *prints all single digit number of base 10
+ *starting from zere, following by a new line
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
