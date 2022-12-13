6881e8a656528b50bd7d44bb6d2204059c1c1cae
AbdulazizGhoniem7 <abdulazizghoniem7@gmail.com>
2022-12-12 16:55:57 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * prints all signle digit number of base 10
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
