13e570867b8987d92abe1edb47f33fbade8b9504
AbdulazizGhoniem7 <Abdulazizghoniem@gmail.com>
2023-12-15 15:35:26 -0800

    0x01. C - Variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0,
- *        only using putchar and without char variables.
+ * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0.
  */
@@ -10,9 +9,17 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
+	{
 		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
 
 	putchar('\n');
+
 	return (0);
 }
